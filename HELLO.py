3ceed60a290cac8b4b41f3faa4bd3d8610e07726
Aishwarya K.C <145260245+aishwaryakcasu@users.noreply.github.com>
2023-12-10 13:13:28 +0530

    Add files via upload

---
+++ HELLO.py
@@ -0,0 +1,16 @@
+# Project Name: Version control for configuration files
+# Description: Implement version control for configuration files : Use version control tools like Git to manage 
+and track changes to configuration files.
+# Author: Aishwarya K C,Manasi B S
+
+# Import necessary modules
+# Example:
+# import os
+# import pandas as pd
+
+def main():
+    # Your main code logic goes here
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
